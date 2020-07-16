3551b4b0dba23eb963167a1f5c2ae236c9006bae
Zen Dharma <zendharma10@gmail.com>
2020-07-16 12:39:56 +0700

    Handle responsive on index

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,10 +1,15 @@
 let navigationBar = document.getElementsByTagName("header")[0]
 let appLogo = document.getElementsByClassName("app-logo")[0]
 
-window.onscroll = function() {
-    if (window.pageYOffset > appLogo.offsetTop) {
-        navigationBar.classList.add('header-fixed')
-    } else {
-        navigationBar.classList.remove('header-fixed')
+if (screen.width < 1024) {
+    navigationBar.classList.add('header-fixed')
+} else {
+    window.onscroll = function() {
+        if (window.pageYOffset > appLogo.offsetTop) {
+            navigationBar.classList.add('header-fixed')
+        } else {
+            navigationBar.classList.remove('header-fixed')
+        }
     }
 }
+
