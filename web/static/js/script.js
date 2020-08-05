d8adcab7146164c2eecfd98ea5a6d982858a7c8b
Nikodem Rabuliński <nikodemrabulinski@gmail.com>
2020-08-05 23:30:25 +0200

    header fix

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,15 +1,4 @@
-let navigationBar = document.getElementsByTagName("header")[0]
-let appLogo = document.getElementsByClassName("app-logo")[0]
-
-if (screen.width < 1024) {
-    navigationBar.classList.add('header-fixed')
-} else {
-    window.onscroll = function() {
-        if (window.pageYOffset > appLogo.offsetTop) {
-            navigationBar.classList.add('header-fixed')
-        } else {
-            navigationBar.classList.remove('header-fixed')
-        }
-    }
-}
+document.addEventListener('scroll', () => {
+    document.documentElement.dataset.scroll = window.scrollY
+})
 
